aceda5bfd379dddc372eec5d14b3900b8b5286ad
gideongrossmanHome <gideongrossman@gmail.com>
2025-08-20 00:21:07 -0400

    Update Summary.tsx

--- src/Summary.tsx
+++ src/Summary.tsx
@@ -8,7 +8,7 @@ const Summary = () => {
       <Typography variant="h6">Hi, I'm Gideon.</Typography>
       <ReactTyped
         strings={[
-          "With a knack for distilling large, complex datasets into intuitive, user flows, I've built performant and elegant web applications for innovative startup companies\
+          "With a knack for distilling large, complex datasets into intuitive, user flows, I've spent the past 10 years building performant and elegant web applications for innovative startup companies\
         in industries spanning biotech, logistics, clean energy and AI.<br><br>\
          I'm passionate about leading frontend and fullstack efforts at mission driven companies that have a positive impact on the world.<br><br>\
         When I'm not coding, you may spot me on a bicycle or surfboard or playing a game of chess.",
