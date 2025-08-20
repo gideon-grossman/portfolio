c3899558334884e01a28bb6309048c2ecd410b72
gideongrossmanHome <gideongrossman@gmail.com>
2025-08-19 22:38:11 -0400

    Update Summary.tsx

--- src/Summary.tsx
+++ src/Summary.tsx
@@ -11,7 +11,7 @@ const Summary = () => {
           "With a knack for distilling large, complex datasets into intuitive, user flows, I've built performant and elegant web applications for innovative startup companies\
         in industries spanning biotech, logistics, clean energy and AI.<br><br>\
          I'm passionate about leading frontend and fullstack efforts at mission driven companies that have a positive impact on the world.<br><br>\
-        When I'm not coding, you may spot me on a biking around a city or catching waves on my surfboard or spending time with my family.",
+        When I'm not coding, you may spot me on a bicycle or surfboard or playing a game of chess.",
         ]}
         typeSpeed={5}
         backSpeed={10}
